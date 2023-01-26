be0d91158faaf3dc2550254d092fb18bf4811cb6
SuspiciousPoptarts <aba_826@outlook.com>
2023-01-25 17:24:04 -0800

    fixed child window not actually being child

--- serveelectron.js
+++ serveelectron.js
@@ -1,85 +1,90 @@
 const { create } = require('domain');
-const { app, BrowserWindow, ipcMain, ipcRenderer, webFrame, BrowserView} = require('electron');
+const { app, BrowserWindow, ipcMain, ipcRenderer, webFrame, BrowserView } = require('electron');
 const fs = require('fs');
 const path = require('path');
 const { electron } = require('process');
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      webPreferences: {
-        // nodeIntegration: true,
-        contextIsolation: true,
-        preload: path.join(__dirname, 'preload.js')
-      },
-      width: 1640,
-      height: 960,
-      minWidth: 800,
-      minHeight: 600,
-      backgroundColor: '#000000',
-      autoHideMenuBar: true,
-      show: false
-    })
-  
-    win.loadFile('pages/wikiwrapper.html')
-
-    win.once('ready-to-show', () => {
-      win.show()
-    })
-
-    ipcMain.on("zoom", (event, msg) => {
-      switch(msg) {
-        case "in":
-          win.webContents.setZoomLevel(win.webContents.getZoomLevel()+0.25);
-          break;
-        case "out":
-          win.webContents.setZoomLevel(win.webContents.getZoomLevel()-0.25);
-          break;
-      }
-    })
-
-    ipcMain.on("load", (event, msg) => {
-      win.loadFile('pages/wikiwrapper.html', { query: { q: msg["q"], b: msg["b"] } })
-    });
-
-    ipcMain.on("createChildWindow", (event, msg) => {
-      switch(msg) {
-        case 'theme':
-          createChildWindow("pages/themeeditor.html")
-          break;
-      }
-    });
-
-    ipcMain.on("theme", (event, msg) => {
-      win.webContents.reloadIgnoringCache();
-    })
-};
-
-const createChildWindow = (src) => {
   const win = new BrowserWindow({
     webPreferences: {
       // nodeIntegration: true,
       contextIsolation: true,
       preload: path.join(__dirname, 'preload.js')
     },
-    width: 864,
+    width: 1640,
     height: 960,
     minWidth: 800,
     minHeight: 600,
-    resizable: false,
     backgroundColor: '#000000',
     autoHideMenuBar: true,
     show: false
   })
-  win.loadFile(src)
+
+  const createChildWindow = (src) => {
+    const winChild = new BrowserWindow({
+      parent: win,
+      webPreferences: {
+        // nodeIntegration: true,
+        contextIsolation: true,
+        preload: path.join(__dirname, 'preload.js')
+      },
+      width: 864,
+      height: 960,
+      minWidth: 800,
+      minHeight: 600,
+      resizable: false,
+      backgroundColor: '#000000',
+      autoHideMenuBar: true,
+      show: false
+    })
+    winChild.loadFile(src)
+
+    winChild.once('ready-to-show', () => {
+      winChild.show()
+    })
+
+    winChild.on("close", event => {
+      event.preventDefault();
+      winChild.hide();
+    })
+
+    ipcMain.on("theme", (event, msg) => {
+      winChild.webContents.reloadIgnoringCache();
+    })
+  };
+
+  win.loadFile('pages/wikiwrapper.html')
 
   win.once('ready-to-show', () => {
     win.show()
   })
 
-  win.on("close", event => {
-    event.preventDefault();
-    win.hide();
+  win.on("close", () => {
+    app.quit();
+  });
+
+  ipcMain.on("zoom", (event, msg) => {
+    switch (msg) {
+      case "in":
+        win.webContents.setZoomLevel(win.webContents.getZoomLevel() + 0.25);
+        break;
+      case "out":
+        win.webContents.setZoomLevel(win.webContents.getZoomLevel() - 0.25);
+        break;
+    }
   })
+
+  ipcMain.on("load", (event, msg) => {
+    win.loadFile('pages/wikiwrapper.html', { query: { q: msg["q"], b: msg["b"] } })
+  });
+
+  ipcMain.on("createChildWindow", (event, msg) => {
+    switch (msg) {
+      case 'theme':
+        createChildWindow("pages/themeeditor.html")
+        break;
+    }
+  });
 
   ipcMain.on("theme", (event, msg) => {
     win.webContents.reloadIgnoringCache();
@@ -91,5 +96,5 @@ ipcMain.on("theme", (event, msg) => {
 })
 
 app.whenReady().then(() => {
-    mainWindow = createWindow()
+  mainWindow = createWindow()
 });
