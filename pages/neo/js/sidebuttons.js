2120052b35c525bb3cbca0774375cb684dbf598d
SuspiciousPoptarts <aba_826@outlook.com>
2023-02-23 22:02:49 -0800

    char-arti list impl

--- pages/neo/js/sidebuttons.js
+++ pages/neo/js/sidebuttons.js
@@ -1,7 +1,137 @@
 
 // SECTION Lists
 
+$("#char").click(async() => {
+    $("#content-window").fadeOut(125);
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
 
+    $("#content-window").html(`${await charList()}`).hide();
+    $("#content-window").fadeIn(125);
+
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
+})
+
+async function charList() {
+    let charList = `
+    <div class="margin-16">
+        <table class="w100p">
+            <tr>
+                <th class="w64">Image</th>
+                <th>Name</th>
+                <th>Element</th>
+                <th>Weapon</th>
+                <th>Substat</th>
+                <th>Stars</th>
+            </tr>`;
+    for(let character of await window.api.getCharList()) {
+        let charInfo = JSON.parse((await window.api.queryGenshinDB(character))[1]);
+        charList += `
+        <tr>
+            <td>
+                <image src="${charInfo["images"]["icon"]}" class="h64 w64 no-shadow">
+            </td>
+            <td class="t-left">
+                <a onclick="window.api.queryGenshinDBRender('${charInfo["name"]}')">${charInfo["fullname"]}</a>
+            </td>
+            <td>${charInfo["element"]}</td>
+            <td>${charInfo["weapontype"]}</td>
+            <td>${charInfo["substat"]}</td>
+            <td>${charInfo["rarity"]}</td>
+        </tr>
+        `;
+    }
+    charList += `</table></div>`;
+    return charList;
+}
+
+$("#wep").click(async() => {
+    $("#content-window").fadeOut(125);
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
+
+    $("#content-window").html(`${await wepList()}`).hide();
+    $("#content-window").fadeIn(125);
+
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
+})
+
+async function wepList() {
+    let wepList = `
+    <div class="margin-16">
+        <table class="w100p">
+            <tr>
+                <th class="w64">Image</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Substat</th>
+                <th>Stars</th>
+            </tr>`;
+    for(let weapon of await window.api.getWepList()) {
+        // ?  'filter' is defined in query.js
+        let wepInfo = JSON.parse((await window.api.queryGenshinDB(weapon))[1]);
+        wepList += `
+        <tr>
+            <td>
+                <image src="${wepInfo["images"]["icon"]}" class="h64 w64 no-shadow">
+            </td>
+            <td class="t-left">
+                <a onclick="window.api.queryGenshinDBRender('${wepInfo["name"].replaceAll(filter, '')}')">${wepInfo["name"]}</a>
+            </td>
+            <td>${wepInfo["weapontype"]}</td>
+            <td>${wepInfo["substat"]}</td>
+            <td>${wepInfo["rarity"]}</td>
+        </tr>
+        `;
+    }
+    wepList += `</table></div>`;
+    return wepList;
+}
+
+$("#arti").click(async() => {
+    $("#content-window").fadeOut(125);
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
+
+    $("#content-window").html(`${await artiList()}`).hide();
+    $("#content-window").fadeIn(125);
+
+    // ? Set Accent to Default
+    $(":root").get(0).style.setProperty("--accent-color", "");
+})
+
+async function artiList() {
+    let artiList = `
+    <div class="margin-16">
+        <table class="w100p">
+            <tr>
+                <th class="w64">Image</th>
+                <th>Name</th>
+                <th colspan="2">Set Bonus</th>
+                <th>Stars</th>
+            </tr>`;
+    for(let artifact of await window.api.getArtiList()) {
+        // ?  'filter' is defined in query.js
+        let artiInfo = JSON.parse((await window.api.queryGenshinDB(artifact))[1]);
+        artiList += `
+        <tr>
+            <td>
+                <image src="${(artiInfo["images"]["flower"])? artiInfo["images"]["flower"]:artiInfo["images"]["circlet"]}" class="h64 w64 no-shadow">
+            </td>
+            <td class="t-left">
+                <a onclick="window.api.queryGenshinDBRender('${artiInfo["name"].replaceAll(filter, '')}')">${artiInfo["name"]}</a>
+            </td>
+            <td class="t-left">${(artiInfo["2pc"])? artiInfo["2pc"]:artiInfo["1pc"]}</td>
+            <td class="t-left">${(artiInfo["4pc"])? artiInfo["4pc"]:""}</td>
+            <td>${(artiInfo["rarity"][artiInfo["rarity"].length-1])}</td>
+        </tr>
+        `;
+    }
+    artiList += `</table></div>`;
+    return artiList;
+}
 
 // !SECTION Lists
 // SECTION Theme
